a68213b75736d53af96421d226b75541cc7ef4ea
Guest User <Guest@epicodus-12.local>
2017-08-15 10:57:59 -0700

    will display answers on question page

--- app/models/answer.js
+++ app/models/answer.js
@@ -2,5 +2,6 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
   author: DS.attr(),
-  explanation: DS.attr()
+  explanation: DS.attr(),
+  question: DS.belongsTo('question', { async: true})
 });
